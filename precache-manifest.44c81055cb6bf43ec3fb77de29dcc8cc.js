c75751177839342b163f8b452da5107dad09481c
glisselisbethjm <glisse1986.jm@outlook.com>
2019-06-22 00:52:24 -0500

    Updates

---
+++ precache-manifest.44c81055cb6bf43ec3fb77de29dcc8cc.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f1f598bb68b16cc93a4eb42934528d",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4c4faa8098780a21d0ff",
+    "url": "/website/static/css/2.303c9422.chunk.css"
+  },
+  {
+    "revision": "6f76fbb2bae5cd0cb9d9",
+    "url": "/website/static/css/main.a9b48cef.chunk.css"
+  },
+  {
+    "revision": "4c4faa8098780a21d0ff",
+    "url": "/website/static/js/2.c22fc2b8.chunk.js"
+  },
+  {
+    "revision": "6f76fbb2bae5cd0cb9d9",
+    "url": "/website/static/js/main.ff155870.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "03fb10a476dec50d05a4907cdcb2f9ee",
+    "url": "/website/static/media/CV-GLISSELISBETH.03fb10a4.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3c815a4b0627cbacd15a7a09c0e2b695",
+    "url": "/website/static/media/icomoon.3c815a4b.woff"
+  },
+  {
+    "revision": "59809af617d58f5da50fb027f3c617ef",
+    "url": "/website/static/media/icomoon.59809af6.ttf"
+  },
+  {
+    "revision": "81edc20628720a70eec4bb593dc06041",
+    "url": "/website/static/media/icomoon.81edc206.svg"
+  },
+  {
+    "revision": "8ba3d2f45fb8fb79875871d14df2c703",
+    "url": "/website/static/media/icomoon.8ba3d2f4.eot"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/website/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/website/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "98d62b1e5f5b556facf319b19c6c7cba",
+    "url": "/website/static/media/lg.98d62b1e.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/website/static/media/lg.ecff1170.eot"
+  },
+  {
+    "revision": "1c87f16b386bbfdc1ae1a5149635cdfa",
+    "url": "/website/static/media/librebaskerville-bold-webfont.1c87f16b.woff2"
+  },
+  {
+    "revision": "c4711930c3a7eff1e02cb9e552bcd386",
+    "url": "/website/static/media/librebaskerville-bold-webfont.c4711930.woff"
+  },
+  {
+    "revision": "4ba92be4076c08dbd7f29d5a4b571c93",
+    "url": "/website/static/media/librebaskerville-italic-webfont.4ba92be4.woff2"
+  },
+  {
+    "revision": "70e8ec15e18a315451177c4074066b3f",
+    "url": "/website/static/media/librebaskerville-italic-webfont.70e8ec15.woff"
+  },
+  {
+    "revision": "520ff79ac50eb226b24a261c0369bfe0",
+    "url": "/website/static/media/librebaskerville-regular-webfont.520ff79a.woff2"
+  },
+  {
+    "revision": "c1d1c75cc10d62fbada454b8f48539b0",
+    "url": "/website/static/media/librebaskerville-regular-webfont.c1d1c75c.woff"
+  },
+  {
+    "revision": "34fac8b61be1cb2b8c6fab656327f658",
+    "url": "/website/static/media/montserrat-bold-webfont.34fac8b6.woff2"
+  },
+  {
+    "revision": "b019c4fcda189cced289b08621a2a9fd",
+    "url": "/website/static/media/montserrat-bold-webfont.b019c4fc.woff"
+  },
+  {
+    "revision": "0891dad6bdd16bcfd31e25eccbd88890",
+    "url": "/website/static/media/montserrat-regular-webfont.0891dad6.woff2"
+  },
+  {
+    "revision": "fa6c61d2daa799fdb2aa14262c0cd6aa",
+    "url": "/website/static/media/montserrat-regular-webfont.fa6c61d2.woff"
+  }
+]);
